075407b4a997f1604cf00f43e437b4205d720feb
unixzii <unixzii@gmail.com>
2019-03-24 01:13:45 +0800

    Add supports for SSR and PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,41 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
-        name: "markdown-pages",
+        name: 'markdown-pages',
       },
     },
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Cyandev',
+        short_name: 'Cyandev',
+        start_url: '/',
+        display: 'standalone',
+        crossOrigin: 'use-credentials'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        runtimeCaching: [
+          {
+            urlPattern: /(\.js$|\.css$)/,
+            handler: 'cacheFirst'
+          },
+          {
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: 'staleWhileRevalidate',
+          },
+          {
+            urlPattern: /fonts\.googleapis\.com\/css/,
+            handler: 'staleWhileRevalidate'
+          },
+        ],
+      }
+    }
   ]
 };
