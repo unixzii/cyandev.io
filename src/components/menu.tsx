14e705d9e645cbd248de66c71af85482130424b5
Cyandev <unixzii@gmail.com>
2023-04-22 22:17:39 +0800

    Optimize visual effects for menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -42,7 +42,7 @@ export default function Menu<T>(props: MenuProps<T>) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-75"
       >
-        <HeadlessMenu.Items className="absolute bottom-12 right-0 w-40 px-1.5 py-1.5 origin-bottom-right rounded-xl backdrop-blur bg-background/60 border border-border shadow-lg ring-0 outline-none select-none">
+        <HeadlessMenu.Items className="absolute bottom-12 right-0 w-40 px-1.5 py-1.5 origin-bottom-right rounded-xl backdrop-blur bg-background/60 border border-border shadow-[0_8px_32px_10px_rgba(0,0,0,0.8)] ring-0 outline-none select-none">
           <_MenuOpenStateListener
             onOpen={props.onOpen}
             onClose={props.onClose}
