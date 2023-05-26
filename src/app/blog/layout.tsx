a42fb7af86798fa30f605051acb7c1477e529c84
Cyandev <unixzii@gmail.com>
2023-05-26 23:46:33 +0800

    Add RSS link to the footer

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -2,13 +2,14 @@ import { PropsWithChildren } from "react";
 import { NavBar } from "@/components/nav";
 import { Footer } from "@/components/footer";
 import { ReadableArea } from "@/components/adaptive-containers";
+import { SlotProvider } from "@/components/slot";
 
 export default function BlogLayout({ children }: PropsWithChildren) {
   return (
-    <>
+    <SlotProvider>
       <NavBar />
       <ReadableArea hasVerticalMargins>{children}</ReadableArea>
       <Footer />
-    </>
+    </SlotProvider>
   );
 }
